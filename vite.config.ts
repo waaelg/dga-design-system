303dea2f3dc3309efa254e8dbbc01b0e3239d59b
waelalghamdi <waaelg@gmail.com>
2025-10-18 21:44:34 +0300

    setup for npm link

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,20 @@
 import { defineConfig } from "vite";
 
 export default defineConfig({
+  build: {
+    rollupOptions: {
+      output: {
+        entryFileNames: "assets/[name].js", // Remove hash from JS
+        chunkFileNames: "assets/[name].js", // Remove hash from chunks
+        assetFileNames: "assets/[name].[ext]", // Remove hash from CSS/assets
+      },
+    },
+  },
   css: {
     preprocessorOptions: {
       scss: {
-        quietDeps: true, // Suppresses deprecation warnings from dependencies
-        silenceDeprecations: ["import"], // Specifically silence @import deprecation warnings
+        quietDeps: true,
+        silenceDeprecations: ["import"],
       },
     },
   },
